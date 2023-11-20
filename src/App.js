bce8a93772f0fcedd01046216de20a285c73ce0f
allanisjoiceelectraj.sumaya <allanissumaya22@gmail.com>
2023-11-19 21:58:27 -0500

    Lab9

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Personlist from './components/Personlist';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-title"><h1>Person List</h1></div>
+      <Personlist />
+    </div>
+  );
+}
+
+export default App;
